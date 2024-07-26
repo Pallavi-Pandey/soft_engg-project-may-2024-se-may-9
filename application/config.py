a0d9100bc71c919269190665ec1987146f2ea156
SONU SINGH <smalik2811@gmail.com>
2024-07-26 13:18:18 +0530

    Refined the Model classes to work with the PostgreSQL database.
    Did the setup of the Database in the project.
    
    Modified the hello_world() function to fetch and display data from database to check the database connection.
    
    Added the Database Schema diagram and Datbase Backup file in the db_directory Folder.

--- application/config.py
+++ application/config.py
@@ -3,7 +3,9 @@
 class Config():
     DEBUG = False
     SECRET_KEY = None
+    SQLALCHEMY_DATABASE_URI = None
 
 class LocalDevelopmentConfig(Config):
     DEBUG = True
     SECRET_KEY = "dev"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:tiger@localhost:5432/student_learner_portal_database" #postgresql://<USERNAME>:<PASSWORD>@<URL>:<PORT_NUMBER>/<DATABASE_NAME>
