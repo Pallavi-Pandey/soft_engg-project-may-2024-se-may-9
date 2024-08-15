645cd424e14f53e2da348ee7c19525e9f139bb22
adigulalkari <adigulalkari825@gmail.com>
2024-08-15 13:40:07 +0530

    Updated landing and signup css

--- static/router.js
+++ static/router.js
@@ -1,5 +1,5 @@
 import course_info from './course_info.js'
-import first_page from './first_page.js'
+import landing from './landing.js'
 import login from './login.js'
 import signup from './signup.js'
 import home from './home.js'
@@ -7,7 +7,7 @@ import weekly_content from './weekly_content.js'
 import course_page from './course_page.js'
 
 const routes = [
-    { path: '/', component: first_page },
+    { path: '/', component: landing },
     { path: '/login', component: login },
     { path: '/signup', component: signup },
     { path: '/course_info', component: course_info },
