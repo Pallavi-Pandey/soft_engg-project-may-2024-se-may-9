f8211df943275e6a0890fd3655802b02c0b19066
adigulalkari <adigulalkari825@gmail.com>
2024-08-17 21:16:31 +0530

    Dynamically updating course cards

--- static/home.js
+++ static/home.js
@@ -2,7 +2,7 @@ import CourseCard from "./Components/CourseCard.js";
 import Navbar from './Components/Navbar.js';
 
 export default {
-    template: `
+  template: `
     <div>
       <Navbar :userEmail="userEmail" />
       <div v-if="user_found">
@@ -10,9 +10,11 @@ export default {
           <h1 class="mb-5 display-4">Welcome, {{ name }}!</h1>
           <div style="justify-content: center">
             <div class="course-cards-container">
-              <CourseCard />
-              <CourseCard />
-              <CourseCard />
+              <CourseCard 
+                v-for="courseId in courseIds" 
+                :key="courseId" 
+                :courseId="courseId" 
+              />
             </div>
           </div>
         </div>
@@ -27,63 +29,63 @@ export default {
         </div>
       </div>
     </div>
-    `,
-    components: {
-      CourseCard,
-      Navbar
-    },
-    data() {
-      return {
-        user_found: true, // Whether logged-in user could be retrieved
-        name: '', // Name of currently logged-in user
-        userId: 0, // ID of currently logged-in user
-        userEmail: '', // Email of currently logged-in user
-        token: localStorage.getItem('authToken') // Authentication token
-      };
-    },
-    methods: {
-      async fetchUserData() {
-        // Retrieve details of currently logged-in user
-        const response = await fetch('/get_user', {
-          headers: {
-            'Authentication-Token': this.token,
-            "Content-Type": "application/json"
-          }
-        }); 
-        if (response.ok) {
-          const userData = await response.json();
-          this.name = userData.name;
-          this.userId = userData.student_id;
-          this.userEmail = userData.email; // Set the user's email
-        } else {
-          this.user_found = false; // User couldn't be retrieved
-          console.error('Failed to fetch user data');
+  `,
+  components: {
+    CourseCard,
+    Navbar
+  },
+  data() {
+    return {
+      user_found: true, // Whether logged-in user could be retrieved
+      name: '', // Name of currently logged-in user
+      userId: 0, // ID of currently logged-in user
+      userEmail: '', // Email of currently logged-in user
+      token: localStorage.getItem('authToken'), // Authentication token
+      courseIds: [1, 2] // List of course IDs for demonstration
+    };
+  },
+  methods: {
+    async fetchUserData() {
+      // Retrieve details of currently logged-in user
+      const response = await fetch('/get_user', {
+        headers: {
+          'Authentication-Token': this.token,
+          "Content-Type": "application/json"
         }
-      },
-      // Log out the user
-      async log_out() {
-        const response = await fetch("/log_out", {
-            method: "POST",
-            headers: {
-              'Authentication-Token': this.token,
-            },
-        });
-
-        if (response.ok) {
-            localStorage.removeItem('authToken');
-            console.log('Logout successful');
-
-            // Redirect to the login page
-            this.$router.push('/login');
-        } else {
-            const responseData = await response.json();
-            console.error('Logout failed:', responseData.error_message);
-        }
+      });
+      if (response.ok) {
+        const userData = await response.json();
+        this.name = userData.name;
+        this.userId = userData.student_id;
+        this.userEmail = userData.email; // Set the user's email
+      } else {
+        this.user_found = false; // User couldn't be retrieved
+        console.error('Failed to fetch user data');
       }
     },
-    created() {
-      // Fetch user data when the component is created
-      this.fetchUserData();
-    }
-};
+    // Log out the user
+    async log_out() {
+      const response = await fetch("/log_out", {
+        method: "POST",
+        headers: {
+          'Authentication-Token': this.token,
+        },
+      });
 
+      if (response.ok) {
+        localStorage.removeItem('authToken');
+        console.log('Logout successful');
+
+        // Redirect to the login page
+        this.$router.push('/login');
+      } else {
+        const responseData = await response.json();
+        console.error('Logout failed:', responseData.error_message);
+      }
+    }
+  },
+  created() {
+    // Fetch user data when the component is created
+    this.fetchUserData();
+  }
+};
